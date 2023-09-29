e28ca4fa4aadc927d6a43c4cdc280cd59fd1b7ac
josecosta-on <35690195+josecosta-on@users.noreply.github.com>
2023-09-29 09:40:39 +0100

    Update ionic-env.sh

--- ionic-env.sh
+++ ionic-env.sh
@@ -2,3 +2,20 @@
 function ionic-start {
     ionic start $1 tabs --type=ionic-angular --cordova --no-interactive
 }
+
+function ion-sign {
+    platform=$ANDROID_HOME/build-tools/$PLATFORM_ENV
+    cordova build android --release -- --packageType=apk
+    UNSIGNED_APK=platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk
+    ${platform}zipalign -v 4 $UNSIGNED_APK app-temp-signed.apk
+    ${platform}apksigner sign --verbose \
+        --v1-signing-enable=true \
+        --v2-signing-enable=true \
+        --v3-signing-enable=false \
+        --ks-pass pass:${PASS_ENV} \
+        --key-pass pass:${PASS_ENV} \
+        --out app.apk \
+        app-temp-signed.apk
+
+
+}
